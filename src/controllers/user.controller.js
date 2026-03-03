d2bdedd3e3098b4bc1e71e7d29695c6a9954d6c7
Razaul Mustafa <razaul@softechproducts.com>
2026-03-03 11:51:51 +0530

    ye to naya branch h

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,21 @@
+// dsfsdafsdaf
+
+import User from '../models/user.model.js';
+// fdsafdsafdsaf
+
+/*
+export const createUser = async (req, res) => {
+    try {
+        const { name, email } = req.body;
+        const newUser = new User({ name, email });
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating user', error });
+    }
+
+    */
+
+    erfwsfdf
+
+    sdfsdafdsafdsaf
